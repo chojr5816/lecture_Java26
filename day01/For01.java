1cc95f0f92fade196a631a7c22c861b797b88589
chojr5816 <chojr3312@gmail.com>
2026-08-20 18:05:35 +0900

    Add files via upload

---
+++ day01/For01.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.util.Scanner;
+
+public class For01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		int sum = 0;
+		
+		for (int i = 1; i <= num; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1부터 " + num + "까지의 합은 " + sum + "입니다.");
+		
+		sc.close();
+	}
+
+}
