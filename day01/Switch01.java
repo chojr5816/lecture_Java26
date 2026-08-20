ef942050274f8e83b1b8d1369db2779d506363c8
chojr5816 <chojr3312@gmail.com>
2026-08-20 18:42:40 +0900

    Add files via upload

---
+++ day01/Switch01.java
@@ -0,0 +1,23 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Switch01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		switch (num % 2) {
+		case 0:
+			System.out.print("짝수입니다.");
+			break;
+		case 1:
+			System.out.println("홀수입니다.");
+			break;
+		}
+	}
+}
---
+++ day01/Switch02.java
@@ -0,0 +1,31 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Switch02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("등급을 입력하세요 : ");
+		String grade = sc.nextLine();
+		
+		switch (grade) {
+		case "A", "a":
+		case "B", "b":
+			System.out.println(">> 아주 열심히 하셨군요~~~!!");
+			break;
+		case "C", "c":
+			System.out.println(">> 남들만큼 하셨네요~~~");
+			break;
+		case "D", "d":
+			System.out.println(">> 조금 더 노력이 필요합니다.");
+			break;
+		case "F", "f":
+			System.out.println(">> 교수실로 찾아오세요");
+			break;
+		}
+	}
+
+}
