b78763c6ed9fc41868f9adfde2ee06b07ee40493
chojr5816 <chojr3312@gmail.com>
2026-08-20 19:15:55 +0900

    Add files via upload

---
+++ day01/Star2.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Star2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 5;
+
+		for (int i = num; i >= 1; i--) {
+		    for (int j = 1; j <= i; j++) {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+
+}
