bdf71335261022a549b94f59e778d62c482f2a3d
chojr5816 <chojr3312@gmail.com>
2026-08-20 17:46:59 +0900

    Add files via upload

---
+++ day01/While01.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.Scanner;
+
+public class While01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		
+		System.out.print("숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		while (num != 0) {
+			sum += num;
+			System.out.print("숫자를 입력하세요 : ");
+			num = sc.nextInt();
+		}
+		
+		System.out.println("지금까지 입력한 숫자의 합은 " + sum + "입니다.");
+		
+		sc.close();
+	}
+
+}
