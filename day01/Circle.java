15c6316a2cce6e06b0b6b1e11dbecc09cdec3ad6
chojr5816 <chojr3312@gmail.com>
2026-08-20 18:44:58 +0900

    Add files via upload

---
+++ day01/Circle.java
@@ -0,0 +1,23 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		final double PI = 3.14;
+		
+		System.out.print("원의 반지름을 입력하세요 : ");
+		double radius = sc.nextDouble();
+		
+		double area = radius * radius * PI;
+		
+		System.out.printf("원의 넓이는 %.2fcm입니다.\n", area);
+		
+		sc.close();
+	}
+
+}
